2549215bec05b174d96691b30ed4b0708fad1752
Alex <alexom003@hotmail.fr>
2026-04-16 16:43:30 +0200

    feat(tests): add tests

---
+++ src/tests/order.test.js
@@ -0,0 +1,8 @@
+const { describe, it } = require('node:test');
+const assert = require('node:assert');
+
+describe('Orders data', () => {
+  it('should have true equal true', () => {
+    assert.ok(true == false);
+  })
+})
